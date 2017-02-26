779f106fbe3978f64bd637359f5e44af6f6dfe1e
Antti Laiti <antti@laiti.fi>
2017-02-26 21:02:55 +0200

    More verbose output

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ var clapConfig = require('./config/clap.json');
 // Start clap detection
 clapDetector.start(clapConfig);
 
-console.log('Notta Klapajaa started');
+console.log('Notta Klapajaa started. Clap ' + clapConfig.CLAPS + ' times in ' + clapConfig.TIMEOUT + ' milliseconds to toggle pause.');
+console.log('')
 
 var client = komponist.createConnection(mpdConfig.port, mpdConfig.server, function() {
    client.password(mpdConfig.pass, function(err) {
        clapDetector.onClaps(clapConfig.CLAPS, clapConfig.TIMEOUT, function(delay) {
            client.toggle();
-           console.log('Pause toggled');
+           console.log(new Date().toISOString() + ': pause toggled');
        });
    });
 });
