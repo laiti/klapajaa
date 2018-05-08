32ecbde7ac2aae818db48710b60fd3b8962c97e5
Antti Laiti <antti@laiti.fi>
2018-05-08 13:03:58 +0300

    let instead of var

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var clapDetector = require('clap-detector');
-var komponist = require('komponist');
+let clapDetector = require('clap-detector');
+let komponist = require('komponist');
 
-var mpdConfig = require('./config/mpd.json');
-var clapConfig = require('./config/clap.json');
+let mpdConfig = require('./config/mpd.json');
+let clapConfig = require('./config/clap.json');
 
 // Start clap detection
 clapDetector.start(clapConfig);
@@ -10,7 +10,7 @@ clapDetector.start(clapConfig);
 console.log('Notta Klapajaa started. Clap ' + clapConfig.CLAPS + ' times in ' + clapConfig.TIMEOUT + ' milliseconds to toggle pause.');
 console.log('')
 
-var client = komponist.createConnection(mpdConfig.port, mpdConfig.server, function() {
+let client = komponist.createConnection(mpdConfig.port, mpdConfig.server, function() {
    client.password(mpdConfig.pass, function(err) {
        clapDetector.onClaps(clapConfig.CLAPS, clapConfig.TIMEOUT, function(delay) {
            client.toggle();
