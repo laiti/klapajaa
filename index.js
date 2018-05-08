ea557d944dbe6a8f4c8b2e22e58ed443bccd012d
Antti Laiti <antti@laiti.fi>
2018-05-08 14:32:03 +0300

    Add errorhandling

--- index.js
+++ index.js
@@ -10,15 +10,24 @@ clapDetector.start(clapConfig);
 console.log('Notta Klapajaa started. Clap ' + clapConfig.CLAPS + ' times in ' + clapConfig.TIMEOUT + ' milliseconds to toggle pause.');
 console.log('')
 
-let client = komponist.createConnection(mpdConfig.port, mpdConfig.server, function() {
-   client.password(mpdConfig.pass, function(err) {
-       if(err) {
-           console.log('Connection failure: cannot connect to ' + mpdConfig.server + ':' + mpdConfig.port + '. Wrong credentials.')
-           return;
-       }
-       clapDetector.onClaps(clapConfig.CLAPS, clapConfig.TIMEOUT, function(delay) {
-           client.toggle();
-           console.log(new Date().toISOString() + ': pause toggled');
-       });
-   });
+let client = komponist.createConnection(mpdConfig.port, mpdConfig.server, function(err) {
+    if (err) {
+        console.log('Connection failure: cannot connect to ' + mpdConfig.server + ':' + mpdConfig.port + '. Server not responding.')
+        return;
+    }
+
+    client.password(mpdConfig.pass, function(err) {
+        if(err) {
+            console.log('Connection failure: cannot connect to ' + mpdConfig.server + ':' + mpdConfig.port + '. Wrong credentials.')
+            return;
+        }
+    });
+
+    clapDetector.onClaps(clapConfig.CLAPS, clapConfig.TIMEOUT, function(delay) {
+        //client.toggle();
+        console.log(new Date().toISOString() + ': Pause toggled.');
+        client.currentsong(function(err, info) {
+            console.log(new Date().toISOString() + ': Playing: ' + info.Artist + ' - ' + info.Title + '.')
+        });
+    });
 });
