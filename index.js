c74db46b5f97d8b0d6c651d8486f4fe46ea49201
Antti Laiti <antti@laiti.fi>
2018-05-08 15:19:11 +0300

    Fix indent and add connectivity error handling

--- index.js
+++ index.js
@@ -10,15 +10,23 @@ clapDetector.start(clapConfig);
 console.log('Notta Klapajaa started. Clap ' + clapConfig.CLAPS + ' times in ' + clapConfig.TIMEOUT + ' milliseconds to toggle pause.');
 console.log('')
 
-let client = komponist.createConnection(mpdConfig.port, mpdConfig.server, function() {
+let client = komponist.createConnection(mpdConfig.port, mpdConfig.server, function(err) {
+    if(err) {
+        console.log('Connection failure: cannot connect to ' + mpdConfig.server + ':' + mpdConfig.port + '. Is mpd running, accessible and configured properly in config/mpd.json?')
+        process.exit()
+    }
+
     client.password(mpdConfig.pass, function(err) {
-       if(err) {
-           console.log('Connection failure: cannot login to ' + mpdConfig.server + ':' + mpdConfig.port + '. Wrong credentials, check config/mpd.json.')
-           process.exit()
-       }
-       clapDetector.onClaps(clapConfig.CLAPS, clapConfig.TIMEOUT, function(delay) {
-           client.toggle();
-           console.log(new Date().toISOString() + ': pause toggled');
-       });
-   });
+        if(err) {
+            console.log('Connection failure: cannot login to ' + mpdConfig.server + ':' + mpdConfig.port + '. Wrong credentials, check config/mpd.json.')
+            process.exit()
+        }
+        clapDetector.onClaps(clapConfig.CLAPS, clapConfig.TIMEOUT, function(delay) {
+            client.toggle();
+
+            client.status(function(err, status) {
+                console.log(new Date().toISOString() + ': pause toggled, status:', status.state);
+            });
+        });
+    });
 });
